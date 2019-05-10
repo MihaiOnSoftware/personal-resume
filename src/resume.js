268b29acf18840cce459badd78652db706dfbef1
Sirealism <m4popesc@ryerson.ca>
2019-05-09 22:19:00 -0400

    After clicking the hamburger, it turns into an X

--- src/resume.js
+++ src/resume.js
@@ -1,22 +1,33 @@
 // resume.js
 
-function toggleHamburger() {
+function toggleHamburger(hamburger) {
+  hamburgerIcon = hamburger.getElementsByTagName("i")[0];
+  hamburgerOpen = hamburgerIcon.classList.contains("fa-times");
+  toggleHamburgerIcon(hamburgerIcon)
+
   navItems = document.getElementsByClassName("nav-item");
 
   notInvisibleNavItems = Array.prototype.filter.call(navItems, function(element) {
     return !element.classList.contains("mobile-invisible");
-  })
+  });
 
   Array.prototype.forEach.call(notInvisibleNavItems, function(element) {
     classes = element.classList;
-    if (classes.contains("mobile-visible")) {
-      classes.remove("mobile-visible");
+    if (hamburgerOpen && classes.contains("mobile-visible")) {
+      classes.remove("mobile-visible");      
     } else {
       classes.add("mobile-visible");
     }
   });
 }
 
-if (typeof module !== 'undefined') {
+function toggleHamburgerIcon(hamburgerIcon) {
+  hamburgerIcon.classList.toggle("fa-bars");
+  hamburgerIcon.classList.toggle("fa");
+  hamburgerIcon.classList.toggle("fas");
+  hamburgerIcon.classList.toggle("fa-times");
+}
+
+if (typeof module !== "undefined") {
   module.exports.toggleHamburger = toggleHamburger;
 }
--- test/resume.test.js
+++ test/resume.test.js
@@ -2,13 +2,19 @@
 const resume = require("../src/resume");
 
 describe("toggleHamburger()", () => {
+  const hamburgerDoc = `
+  <a href="javascript:;" onclick="toggleHamburger(this)" class="hamburger">
+    <i class="fa fa-bars"></i>
+  </a>`;
+
   it("toggles all nav-items to be visible if not visible", () => {
-    document.body.innerHTML = `<div class="nav no-print">
-        <a class="nav-item" style="display: none;">Nav Item 1</a>
-        <a class="nav-item" style="display: none;">Nav Item 2</a>
+    document.body.innerHTML = hamburgerDoc + `
+    <div class="nav no-print">
+        <a class="nav-item">Nav Item 1</a>
+        <a class="nav-item">Nav Item 2</a>
     </div>`;
 
-    resume.toggleHamburger();
+    resume.toggleHamburger(hamburger());
 
     navItems = document.getElementsByClassName("nav-item");
 
@@ -18,12 +24,16 @@ describe("toggleHamburger()", () => {
   });
 
   it("toggles all nav-items to be invisible if visible", () => {
-    document.body.innerHTML = `<div class="nav no-print">
+    document.body.innerHTML = `
+    <a href="javascript:;" onclick="toggleHamburger(this)" class="hamburger">
+      <i class="fas fa-times"></i>
+    </a>
+    <div class="nav no-print">
         <a class="nav-item mobile-visible">Nav Item 1</a>
         <a class="nav-item mobile-visible">Nav Item 2</a>
     </div>`;
-
-    resume.toggleHamburger();
+    
+    resume.toggleHamburger(hamburger());
 
     navItems = document.getElementsByClassName("nav-item");
 
@@ -33,12 +43,14 @@ describe("toggleHamburger()", () => {
   });
 
   it("won't toggle nav-items with mobile-invisible", () => {
-    document.body.innerHTML = `<div class="nav no-print">
+    document.body.innerHTML = hamburgerDoc + `
+    <div class="nav no-print">
         <a class="nav-item">Nav Item 1</a>
         <a class="nav-item mobile-invisible">Nav Item 2</a>
-    </div>`;
+    </div>
+    `;
 
-    resume.toggleHamburger();
+    resume.toggleHamburger(hamburger());
 
     navItems = document.getElementsByClassName("nav-item");
 
@@ -50,4 +62,24 @@ describe("toggleHamburger()", () => {
       }
     });
   });
+
+  it("changes the hamburger into a times", () => {
+    document.body.innerHTML = hamburgerDoc + `
+    <div class="nav no-print">
+        <a class="nav-item" style="display: none;">Nav Item 1</a>
+        <a class="nav-item" style="display: none;">Nav Item 2</a>
+    </div>
+    `;
+
+    resume.toggleHamburger(hamburger());
+
+    navItems = document.getElementsByClassName("nav-item");
+    hamburgerIcon = hamburger().getElementsByTagName('i')[0]
+
+    expect(hamburgerIcon.className).toBe("fas fa-times")
+  });
+
+  function hamburger() {
+    return document.getElementsByClassName("hamburger")[0];
+  }
 });
