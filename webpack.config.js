3a6b37fd0987e74c3c7eab68de209e20eccf62ae
Sirealism <m4popesc@ryerson.ca>
2019-09-14 19:29:29 -0400

    Removing dist from git and making webpack copy files from src over

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./src/faq/time.js",
+  entry: {
+    "faq/time": "./src/faq/time.js",
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin(
+      [
+        {
+          from: "**/*.html",
+        },
+        {
+          from: "**/*.css",
+        },
+      ],
+      { context: "./src" }
+    ),
+  ],
   output: {
-    filename: "faq/time.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
